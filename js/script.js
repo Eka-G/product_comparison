6679abbc759a5934b13aa313f3958c31c6782328
Eka-G <ekaglazkovaa@yandex.ru>
2021-01-14 16:21:09 +0300

    add: content-wrapper

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const getContentForTippy = (el) => el.dataset.tooltip;
 
 tippy('.tooltips', {
   content: getContentForTippy,
-  // '<strong>4500 мАч</strong> - это примерно 30 часов активного использования устройства',
   allowHTML: true,
   maxWidth: 260,
   moveTransition: 'transform 0.2s ease',
